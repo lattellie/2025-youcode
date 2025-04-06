cabc5bb68e3c7a2576602bc429edf6ed6a2332b3
enora <enorasun1120@gmail.com>
2025-04-05 23:04:29 -0700

    added login and register page style, still need google login

--- frontend/app/pages/Register.tsx
+++ frontend/app/pages/Register.tsx
@@ -1,37 +1,120 @@
 // app/pages/Page2.tsx
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+
 
 type Props = {
   navigation: any;
 };
 
 const Register: React.FC<Props> = ({ navigation }) => {
+
+  const [firstName, setFirstName] = React.useState('');
+  const [lastName, setLastName] = React.useState('');
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [email, setEmail] = React.useState('');
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Register</Text>
-      <Button
-        title="Register"
-        onPress={() =>console.log("register!")}
+      <Text style={styles.head}>Register for an account</Text>
+      <Text style={styles.text}>
+        Already have an account?{" "}
+        <Text style={styles.link} onPress={() => navigation.navigate("Login")}>
+          Log in!{" "}
+        </Text>
+      </Text>
+      <TextInput
+        style={styles.input}
+        placeholder="First Name"
+        value={firstName}
+        onChangeText={setFirstName}
       />
-      <Button
-        title="Login"
-        onPress={() => navigation.navigate('Login')}
+      <TextInput
+        style={styles.input}
+        placeholder="Last Name"
+        value={lastName}
+        onChangeText={setLastName}
       />
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        value={username}
+        onChangeText={setUsername}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+        keyboardType="email-address"
+      />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('Register')}
+      >
+        <Text style={styles.buttonText}>Register</Text>
+      </TouchableOpacity>
+
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  link:{
+    fontSize: 14,
+    marginBottom: 20,
+    textAlign: 'left',
+    textDecorationLine: 'underline',
+  },
+  head:{
+    fontSize: 24,
+    marginTop: 20,
+    marginBottom: 20,
+    fontWeight: 'bold',
+    color: '#333',
+    textAlign: 'center',
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
   text: {
-    fontSize: 24,
+    fontSize: 14,
     marginBottom: 20,
+    textAlign: 'left',
   },
+  input: {
+    borderWidth: 1,
+    borderColor: '#aaa',
+    padding: 12,
+    marginVertical: 8,
+    borderRadius: 6,
+    width: '25%'
+  },
+  button: {
+    backgroundColor: '#1F1F1F',
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderRadius: 6,
+    marginVertical: 20,
+    width: '50%',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: '600',
+  },
+
 });
 
 export default Register;
