f24fa9d6347da909dfaaeaaf9aaea9a4ac95572d
enora <enorasun1120@gmail.com>
2025-04-06 00:29:31 -0700

    added alternative login method login, register visual done need to add api

--- frontend/app/pages/Login.tsx
+++ frontend/app/pages/Login.tsx
@@ -1,6 +1,7 @@
 // app/pages/Page2.tsx
 import React from 'react';
-import { View, Text, Button, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import {View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
 
 type Props = {
   navigation: any;
@@ -15,11 +16,14 @@ const Login: React.FC<Props> = ({ navigation }) => {
     <View style={styles.container}>
       <Text style={styles.head}>Login to your account</Text>
       <Text style={styles.text}>
-              Doesn't have an account?{" "}
-              <Text style={styles.link} onPress={() => navigation.navigate("Register")}>
-                Sign up!{" "}
-              </Text>
-            </Text>
+        Doesn't have an account?{" "}
+        <Text
+          style={styles.link}
+          onPress={() => navigation.navigate("Register")}
+        >
+          Sign up!{" "}
+        </Text>
+      </Text>
       <TextInput
         style={styles.input}
         placeholder="Username"
@@ -35,9 +39,28 @@ const Login: React.FC<Props> = ({ navigation }) => {
       />
       <TouchableOpacity
         style={styles.button}
-        onPress={() => navigation.navigate('MainPage')}
+        onPress={() => navigation.navigate("MainPage")}
       >
         <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
+
+      <View style={styles.separatorContainer}>
+        <View style={styles.line} />
+        <Text style={styles.separatorText}>Continue with</Text>
+        <View style={styles.line} />
+      </View>
+
+      <TouchableOpacity style={styles.facebookButton}>
+        <FontAwesome name="facebook" size={16} color="white" />
+        <Text style={styles.fakeButtonText}>Login with Facebook</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.googleButton}>
+        <FontAwesome name="google" size={16} color="#9AA0A6" />
+        <Text style={styles.googleButtonText}>Login with Google</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.appleButton}>
+        <FontAwesome name="apple" size={16} color="white" />
+        <Text style={styles.fakeButtonText}>Login with Apple</Text>
       </TouchableOpacity>
     </View>
   );
@@ -73,14 +96,15 @@ const styles = StyleSheet.create({
     padding: 12,
     marginVertical: 8,
     borderRadius: 6,
-    width: '25%'
+    width: '50%'
   },
   button: {
     backgroundColor: '#1F1F1F',
     paddingVertical: 12,
     paddingHorizontal: 24,
     borderRadius: 6,
-    marginVertical: 20,
+    marginVertical: 90,
+    marginTop: 30,
     width: '50%',
     alignItems: 'center',
   },
@@ -89,6 +113,69 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: '600',
   },
+  facebookButton: {
+    flexDirection: 'row',
+    backgroundColor: '#1877F2',
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderRadius: 6,
+    marginBottom: 1,
+    width: '50%',
+    alignItems: 'center',
+  },
+  googleButton: {
+    flexDirection: 'row',
+    backgroundColor: '#ffffff',
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderRadius: 6,
+    marginVertical: 10,
+    marginBottom: 1,
+    width: '50%',
+    alignItems: 'center',
+  },
+  fakeButtonText: {
+    marginLeft: 8,
+    color: 'white',
+    fontSize: 16,
+    fontWeight: '600',
+  },
+  googleButtonText: {
+    marginLeft: 8,
+    color: '#9AA0A6',
+    fontSize: 16,
+    fontWeight: '600',
+  },
+  appleButton: {
+    flexDirection: 'row',
+    backgroundColor: '#1F1F1F',
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderRadius: 6,
+    marginVertical: 10,
+    width: '50%',
+    alignItems: 'center',
+  },
+  logo: {
+    width: 10,
+    height: 10,
+    marginBottom: 10,
+  },
+  separatorContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 16,
+  },
+  line: {
+    height: 1,
+    width: '10%',
+    backgroundColor: '#ccc',
+  }, 
+  separatorText: {
+    marginHorizontal: 8,
+    fontSize: 12,
+    color: '#888',
+  }
 
 });
 
